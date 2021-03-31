b6a7a986748ae2dcd4ba3c401475a90564b1d488
zihussaini <zihussaini@honeybeerobotics.com>
2021-03-31 12:22:02 -0400

    added different resolution and multiple monitor support

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class WallpaperImage:
         pixel_r = int(pixel_r + 0.5)
 
         upscale_size = 2*pixel_r*antialias_scale
-        mask = Image.new('RGBA', (upscale_size, upscale_size), self.background_color)
+        mask = Image.new('RGBA', (upscale_size, upscale_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(mask)
         bound = (0, 0, upscale_size, upscale_size)
         draw.ellipse(bound, fill=self.draw_color, outline=self.background_color)
@@ -55,7 +55,7 @@ class WallpaperImage:
             pixel_y - pixel_r,
             pixel_x + pixel_r,
             pixel_y + pixel_r)
-        self.image.paste(mask, box=bound)
+        self.image.paste(mask, box=bound, mask=mask)
 
     def draw_empty_circle(self, pixel_x, pixel_y, pixel_r, thickness):
         antialias_scale = 8
@@ -66,7 +66,7 @@ class WallpaperImage:
         pixel_r = int(pixel_r + 0.5)
 
         upscale_size = 2*pixel_r*antialias_scale
-        mask = Image.new('RGBA', (upscale_size, upscale_size), self.background_color)
+        mask = Image.new('RGBA', (upscale_size, upscale_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(mask)
         bound = (0, 0, upscale_size, upscale_size)
         draw.ellipse(bound, fill=self.draw_color, outline=self.background_color)
@@ -75,7 +75,7 @@ class WallpaperImage:
             antialias_scale*thickness,
             upscale_size - antialias_scale*thickness,
             upscale_size - antialias_scale*thickness)
-        draw.ellipse(bound, fill=self.background_color, outline=self.background_color)
+        draw.ellipse(bound, fill=(0, 0, 0, 0), outline=(0, 0, 0, 0))
         mask = mask.resize((2*pixel_r, 2*pixel_r), Image.ANTIALIAS)
 
         bound = (
@@ -83,50 +83,52 @@ class WallpaperImage:
             pixel_y - pixel_r,
             pixel_x + pixel_r,
             pixel_y + pixel_r)
-        self.image.paste(mask, box=bound)
+        self.image.paste(mask, box=bound, mask=mask)
 
     def draw_scale_horizontal(self, pixels_x, height_ones, height_tens):
         draw = ImageDraw.Draw(self.image)
         for i, pixel_x in enumerate(pixels_x):
-            if i%10 == 0:
-                draw.line((
-                    pixel_x, self.image_height/2-height_tens/2,
-                    pixel_x, self.image_height/2+height_tens/2),
-                    fill="white")
-            else:
-                draw.line((
-                    pixel_x, self.image_height/2-height_ones/2,
-                    pixel_x, self.image_height/2+height_ones/2),
-                    fill="white")
+            height = height_tens if i%10 == 0 else height_ones
+            draw.line((
+                pixel_x, self.image_height/2 - height/2,
+                pixel_x, self.image_height/2 + height/2),
+                fill="white")
         draw.line((0, self.image_height/2, self.image_width, self.image_height/2), fill="white")
 
     def draw_scale_vertical(self, pixel_x, pixels_y, height_ones, height_tens):
         draw = ImageDraw.Draw(self.image)
         for i, pixel_y in enumerate(pixels_y):
             height = height_tens if i%10 == 0 else height_ones
-
-            draw.line((
-                pixel_x + height,
-                self.image_height/2 + pixel_y,
-                pixel_x,
-                self.image_height/2 + pixel_y), fill="white")
-            draw.line((
-                pixel_x + height,
-                self.image_height/2 - pixel_y,
-                pixel_x,
-                self.image_height/2 - pixel_y), fill="white")
+            # draw twice, one for above center and one for below
+            for pixel_y in [pixel_y, -pixel_y]:
+                draw.line((
+                    pixel_x + height/2, self.image_height/2 + pixel_y,
+                    pixel_x - height/2, self.image_height/2 + pixel_y),
+                    fill="white")
         draw.line((pixel_x, 0, pixel_x, self.image_height), fill="white")
 
     def show(self):
         self.image.show()
 
-    def save(self):
-        self.image.save('wallpaper.png')
+    def save(self, monitors=1):
+        """monitors is number of horizontal equal size/resolution monitors"""
+        if monitors < 1:
+            raise Exception("invalid number of monitors")
+        elif monitors == 1:
+            self.image.save('wallpaper.png')
+        else:
+            for i in range(monitors):
+                image_part = self.image.crop((
+                    self.image.width*i/monitors, 0,
+                    self.image.width*(i+1)/monitors, self.image.height))
+                image_part.save(f'wallpaper{i}.png')
 
 
 class CoordinateMapper:
+    """Calculates conversion between pixel coordinates and real coordinates"""
+
     def __init__(self, px0, py0):
-        """px0 is pixel corresponding to 0 x location, similar for py0"""
+        """px0 is pixel corresponding to 0 actual x location, similar for py0"""
         self.dx = px0
         self.sx = None
         self.ex = None
@@ -142,6 +144,9 @@ class CoordinateMapper:
 
     def calc_x(self, px1, ax1, px2, ax2):
         """Calculates x scaling with two pairs of pixel values and actual values"""
+        if ax1 == 0 or ax2 == 0:
+            raise Exception("Do not use ax1 = 0 or ax2 = 0 for calc_x")
+
         A = np.array([
             [1/np.log(ax1), 1],
             [1/np.log(ax2), 1]])
@@ -153,7 +158,10 @@ class CoordinateMapper:
         self.ex = res[1][0]
 
     def calc_y(self, py1, ay1, py2, ay2):
-        """Calculates y scaling with two pairs of pixel values and actual values"""
+        """Calculates y scaling with two pairs of pixel values and actual values."""
+        if ay1 == 0 or ay2 == 0:
+            raise Exception("Do not use ay1 = 0 or ay2 = 0 for calc_y")
+
         A = np.array([
             [1/np.log(ay1), 1],
             [1/np.log(ay2), 1]])
@@ -201,7 +209,6 @@ class CoordinateMapper:
 
 def load_data():
     # parse csv files
-    sun_radius = 696340 # not added to any csv file
     df_planets = pd.read_csv('data/planets.csv')
     df_moons = pd.read_csv('data/moons.csv')
 
@@ -209,7 +216,7 @@ def load_data():
     df_moons.set_index("MOON", inplace=True)
 
     # list of tuples: (location x, location y, radius, solid)
-    circles = [(0, 0, sun_radius, False)]
+    circles = []
 
     for index, row in df_planets.iterrows():
         location_x = row["DIST FROM SUN (km)"]
@@ -222,25 +229,28 @@ def load_data():
         location_x = df_planets["DIST FROM SUN (km)"][row["PLANET"]]
         location_y = row["DIST FROM PLANET (km)"]
         radius = row["RADIUS (km)"]
-        circles.append((location_x, location_y, radius, True))
+        solid = True
+        circles.append((location_x, location_y, radius, solid))
 
     return df_planets, df_moons, circles
 
-def generate_wallpaper(width, height):
+def generate_wallpaper(width, height, wide=False):
+    """When wide is set to True, the x axis scaling uses sun radius as well"""
+    # TODO: add checks to make sure nothing overlaps with set parameters
     # all of these are in pixels
-    SUN_X = 200 # position of sun along x axis
-    MERCURY_X = 400 # distance of mercury from sun along x axis
-    JUPITER_R = 100 # radius of jupiter (largest planet)
-    IAPETUS_Y = 600 # distance of iapetus (furthest moon) along y axis
-    NEPTUNE_X = 3000 # distanc of neptune (furthest planet) along x axis
+    SUN_R = height/6 # radius of sun
+    MERCURY_X = SUN_R*1.5 # distance of mercury from sun center along x axis
+    IAPETUS_Y = height*5/12 # distance of iapetus (furthest moon) along y axis of image from center
+    NEPTUNE_X = width - SUN_R*2 # distance of neptune (furthest planet) from sun center along x axis
+    EMPTY_CIRCLE_THICKNESS = height/120
 
     NUM_STARS = 3000
 
     # all of these are in pixels
-    X_TICKS_HEIGHT_ONES = 20
-    X_TICKS_HEIGHT_TENS = 50
-    Y_TICKS_HEIGHT_ONES = 20
-    Y_TICKS_HEIGHT_TENS = 50
+    X_TICKS_HEIGHT_ONES = height/72
+    X_TICKS_HEIGHT_TENS = height/30
+    Y_TICKS_HEIGHT_ONES = height/72
+    Y_TICKS_HEIGHT_TENS = height/30
 
     background_color = (1, 28, 44)
     draw_color = (249, 123, 35)
@@ -250,14 +260,23 @@ def generate_wallpaper(width, height):
 
 
     """ initialize mapper """
-    mapper = CoordinateMapper(SUN_X, height/2)
+    mapper = CoordinateMapper(SUN_R, height/2)
 
     mapper.calc_x(
-        SUN_X + MERCURY_X, df_planets["DIST FROM SUN (km)"]["Mercury"],
-        SUN_X + NEPTUNE_X, df_planets["DIST FROM SUN (km)"]["Neptune"])
+        SUN_R + MERCURY_X, df_planets["DIST FROM SUN (km)"]["Mercury"],
+        SUN_R + NEPTUNE_X, df_planets["DIST FROM SUN (km)"]["Neptune"])
     mapper.calc_y(
-        height/2 + JUPITER_R, df_planets["RADIUS (km)"]["Jupiter"],
+        height/2 + SUN_R, df_planets["RADIUS (km)"]["Sun"],
         height/2 + IAPETUS_Y, df_moons["DIST FROM PLANET (km)"]["Iapetus"])
+
+    if wide:
+        mapper.calc_x(
+            SUN_R*2, df_planets["RADIUS (km)"]["Sun"],
+            SUN_R + NEPTUNE_X, df_planets["DIST FROM SUN (km)"]["Neptune"])
+    # use same scaling as x axis
+    # mapper.calc_y(
+    #     height/2 + SUN_R + MERCURY_X, df_planets["DIST FROM SUN (km)"]["Mercury"],
+    #     height/2 + SUN_R + NEPTUNE_X, df_planets["DIST FROM SUN (km)"]["Neptune"])
 
     """ make image """
     image = WallpaperImage(width, height, background_color, draw_color)
@@ -274,14 +293,6 @@ def generate_wallpaper(width, height):
         pxys.append((px, py))
     image.add_stars(pxys)
 
-    # draw planets + moons
-    for ax, ay, ar, solid in circles:
-        px, py, pr = mapper.real_to_pixel(ax, ay, ar)
-        if solid:
-            image.draw_circle(px, py, pr)
-        else:
-            image.draw_empty_circle(px, py, pr, 8)
-
     # draw horizontal scale
     ones_x = 5e7
     max_ax = mapper.pixel_to_real(width, mapper.dy, 0)[0]
@@ -294,10 +305,21 @@ def generate_wallpaper(width, height):
     max_ay = mapper.pixel_to_real(mapper.dx, width, 0)[1]
     ays = np.arange(0, max_ay, ones_y)
     pys = [mapper.real_to_pixel(0, ay, 0)[1] - height/2 for ay in ays]
-    image.draw_scale_vertical(SUN_X, pys, Y_TICKS_HEIGHT_ONES, Y_TICKS_HEIGHT_TENS)
+    image.draw_scale_vertical(mapper.dx, pys, Y_TICKS_HEIGHT_ONES, Y_TICKS_HEIGHT_TENS)
+
+    # draw planets + moons
+    for ax, ay, ar, solid in circles:
+        px, py, pr = mapper.real_to_pixel(ax, ay, ar)
+        if solid:
+            image.draw_circle(px, py, pr)
+        else:
+            image.draw_empty_circle(px, py, pr, EMPTY_CIRCLE_THICKNESS)
 
     return image
 
 if __name__ == "__main__":
     image = generate_wallpaper(3440, 1440)
     image.save()
+
+    image = generate_wallpaper(2*1920, 1080, True)
+    image.save(monitors=2)
